0ed54ca219d91b2e43a62f98ab4112447145b89d
yarzardriz-source <yarzardriz@gmail.com>
2025-11-21 12:27:38 +0630

    modeified sectence

--- index.cpp
+++ index.cpp
@@ -3,5 +3,6 @@ using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
+    cout << "This project uses the C++14 language standard version." << endl;   
     return 0;
 }           
